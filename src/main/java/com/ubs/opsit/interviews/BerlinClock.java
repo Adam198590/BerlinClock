599befe611838d83e384ec06ff2927eef3d4327f
Adam198590 <poch.adam.ta@gmail.com>
2016-02-14 13:26:48 +0100

    Create implementation of TimeConverter for berlin clock. Add "before" block to test module with initialization of my new class. As i understand - i have to do it for testing new logic, and its the main task - create and test it. Second task - is create the local repository for represent my steps for create this business logic. But i have to send only source files, so i will create the remote repository to be able read my commit messages. And last one - my solution should be represent as well passed tests. I don`t need to create desktop or web application to show how it works. I understood it from this line "We have created a number of acceptance tests for the Berlin Clock and your challenge is to get them passing." in attached instructions.

---
+++ src/main/java/com/ubs/opsit/interviews/BerlinClock.java
@@ -0,0 +1,12 @@
+package com.ubs.opsit.interviews;
+
+/**
+ * Created by Denis Pavlenko on 14.02.2016.
+ */
+public class BerlinClock implements TimeConverter {
+
+    @Override
+    public String convertTime(String aTime) {
+        return null;
+    }
+}
