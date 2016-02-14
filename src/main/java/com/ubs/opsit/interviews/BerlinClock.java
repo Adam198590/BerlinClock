09888edb8f4f5bab0ca990e06956cbb5acf01bac
Adam198590 <poch.adam.ta@gmail.com>
2016-02-14 14:04:03 +0100

    Probably, would be more comfortable to use separate variables to store string constants and put to special class. Add check to stop working if we have incorrect data format.

--- src/main/java/com/ubs/opsit/interviews/BerlinClock.java
+++ src/main/java/com/ubs/opsit/interviews/BerlinClock.java
@@ -7,6 +7,10 @@ public class BerlinClock implements TimeConverter {
 
     @Override
     public String convertTime(String aTime) {
+        if (!aTime.matches(ConfigClock.TIME_FORMAT)) {
+            throw new IllegalArgumentException("Time must be in the format HH:MM:SS");
+        }
+
         return null;
     }
 }
---
+++ src/main/java/com/ubs/opsit/interviews/ConfigClock.java
@@ -0,0 +1,8 @@
+package com.ubs.opsit.interviews;
+
+/**
+ * Created by Denis Pavlenko on 14.02.2016.
+ */
+public class ConfigClock {
+    public static String TIME_FORMAT = "\\d\\d:\\d\\d:\\d\\d";
+}
