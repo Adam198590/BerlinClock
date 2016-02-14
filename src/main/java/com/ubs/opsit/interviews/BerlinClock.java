c5453780e4b9461e09646391186450b39a8738d9
Adam198590 <poch.adam.ta@gmail.com>
2016-02-14 17:37:48 +0100

    Here i tried to make business logic of Berlin clock as short as possible. Except of it i made all methods enough short too with understandable names.

--- src/main/java/com/ubs/opsit/interviews/BerlinClock.java
+++ src/main/java/com/ubs/opsit/interviews/BerlinClock.java
@@ -1,5 +1,8 @@
 package com.ubs.opsit.interviews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Denis Pavlenko on 14.02.2016.
  */
@@ -8,9 +11,70 @@ public class BerlinClock implements TimeConverter {
     @Override
     public String convertTime(String aTime) {
         if (!aTime.matches(ConfigClock.TIME_FORMAT)) {
-            throw new IllegalArgumentException("Time must be in the format HH:MM:SS");
+            throw new IllegalArgumentException("Time must be in the format HH:MM:SS, but we have - " + aTime);
         }
 
-        return null;
+        List<Integer> parts = new ArrayList<>();
+        for (String part : aTime.split(ConfigClock.SEPARATOR_SIGN)) {
+            parts.add(Integer.parseInt(part));
+        }
+
+        return new StringBuilder().
+                append(getSeconds(parts.get(2))).append("\r\n").
+                append(getTopRowOfHours(parts.get(0))).append("\r\n").
+                append(getBottomRowOfHours(parts.get(0))).append("\r\n").
+                append(getTopRowOfMinutes(parts.get(1))).append("\r\n").
+                append(getBottomRowOfMinutes(parts.get(1))).
+                toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BerlinClock().convertTime("00:00:00"));
+    }
+
+    protected String getSeconds(int value) {
+        if (value % 2 == 0) return ConfigClock.ON_SIGN;
+        else return ConfigClock.OFF_SIGN;
+    }
+
+    protected String getTopRowOfHours(int value) {
+        return getOnOff(ConfigClock.COUNT_OF_TOP_HOURS, getTopNumberOfOnSigns(value));
+    }
+
+    protected String getBottomRowOfHours(int value) {
+        return getOnOff(ConfigClock.COUNT_OF_BOTTOM_HOURS, value % 5);
+    }
+
+    protected String getTopRowOfMinutes(int value) {
+        return getOnOff(
+                ConfigClock.COUNT_OF_TOP_MINUTES,
+                getTopNumberOfOnSigns(value),
+                ConfigClock.ON_SIGN).replaceAll("YYY", "YYR");
+    }
+
+    protected String getBottomRowOfMinutes(int value) {
+        return getOnOff(ConfigClock.COUNT_OF_BOTTOM_MINUTES, value % 5, ConfigClock.ON_SIGN);
+    }
+
+    private String getOnOff(int lampsInSummary, int lampsIsOnCount) {
+        return getOnOff(lampsInSummary, lampsIsOnCount, ConfigClock.QUARTER_SIGN);
+    }
+
+    private String getOnOff(int lampsInSummary, int lampsIsOnCount, String notOffSign) {
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < lampsIsOnCount; i++) {
+            out.append(notOffSign);
+        }
+
+        for (int i = 0; i < (lampsInSummary - lampsIsOnCount); i++) {
+            out.append(ConfigClock.OFF_SIGN);
+        }
+
+        return out.toString();
+    }
+
+    private int getTopNumberOfOnSigns(int value) {
+        return (value - (value % 5)) / 5;
     }
 }
--- src/main/java/com/ubs/opsit/interviews/ConfigClock.java
+++ src/main/java/com/ubs/opsit/interviews/ConfigClock.java
@@ -4,5 +4,22 @@ package com.ubs.opsit.interviews;
  * Created by Denis Pavlenko on 14.02.2016.
  */
 public class ConfigClock {
+
     public static String TIME_FORMAT = "\\d\\d:\\d\\d:\\d\\d";
+
+    public static String SEPARATOR_SIGN = ":";
+
+    public static String ON_SIGN = "Y";
+
+    public static String QUARTER_SIGN = "R";
+
+    public static String OFF_SIGN = "O";
+
+    public static int COUNT_OF_TOP_HOURS = 4;
+
+    public static int COUNT_OF_BOTTOM_HOURS = 4;
+
+    public static int COUNT_OF_TOP_MINUTES = 11;
+
+    public static int COUNT_OF_BOTTOM_MINUTES = 4;
 }
